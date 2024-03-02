4c600458c8eb1366eca1e9d80dbc2bb53afae240
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-03-02 16:24:46 +0400

    - add cursor

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -9,7 +9,7 @@
         <transition name="fade" mode="out-in">
             <div class="grid grid-cols-2 gap-3 auto-rows-fr p-2">
 
-                <div class="p-3 md:h-24 flex justify-center items-center box-decoration-slice bg-gradient-to-r from-blue-400 to-green-500 text-white"
+                <div class="p-3 md:h-24 flex justify-center items-center box-decoration-slice bg-gradient-to-r from-blue-400 to-green-500 text-white cursor-pointer"
                     @click="onCategoryClicked(category)" v-for="category in all_categories" :key="category.id">
                     <span class="font-extrabold">{{ category.name }}</span>
                 </div>
